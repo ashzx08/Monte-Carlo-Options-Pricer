6a7c8d0cca1adea26ad8122b68324cd64a7ead9b
Ashwin Chopra <ashveen2545@icloud.com>
2026-09-07 21:23:45 +0100

    Building the initial simulation without Numpy

--- Random Walk Simulation.py
+++ Random Walk Simulation.py
@@ -1,1 +1,19 @@
-print("hello world")
+import random
+
+Number_of_steps = int(input("Enter the number of steps for the random walk simulation: "))
+
+starting_position = 0
+step_sizes = [1, -1]
+positions = [starting_position]
+current_step = 0
+current_position = starting_position
+final_position = 0
+
+while current_step <= Number_of_steps:
+    step = random.choice(step_sizes)
+    current_position += step
+    positions.append(current_position)
+    current_step += 1
+
+final_position = current_position
+print(positions)
