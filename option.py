ec8a308b8808263ea8e8b6decca37e39709c0266
Ashwin Chopra <ashveen2545@icloud.com>
2026-09-19 03:28:49 +0100

    Using numpy to calculate payoff

--- option.py
+++ option.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 class EuropeanCall:
 
     def __init__(self, strike_price):
         self.strike_price = strike_price
 
     def payoff(self, final_price):
-        return max((final_price - self.strike_price), 0)
+        return np.maximum((final_price - self.strike_price), 0)
